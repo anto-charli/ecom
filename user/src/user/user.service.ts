98e54336d3e9f33e221e76d86f1e8e1db316adb6
Anto Charlii <antocharly.a@skava.com>
2021-09-21 21:11:31 +0530

    Fixed checking if user is available

--- user/src/user/user.service.ts
+++ user/src/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 import { User, UserDocument } from './schema'
 import { UserDto } from './dto'
+import { CreateUserParam } from './typings'
 @Injectable()
 export class UserService {
   constructor(
@@ -10,22 +11,36 @@ export class UserService {
     private readonly userModel: Model<UserDocument>,
   ) {}
 
-  async createUser(params, newUser: UserDto): Promise<User> {
-    let response: User
+  async createUser(params: CreateUserParam, newUser: UserDto): Promise<User> {
+    let response = new User()
     let responseMessage = 'success'
     try {
-      response = await this.userModel.create(newUser)
+      const existingUser = await this.userModel
+        .findOne({ email: newUser?.email })
+        .exec()
+      if (existingUser) {
+        response.error = 'User already exists'
+        response.responseMessage = 'failure'
+      } else {
+        response = await this.userModel.create(newUser)
+      }
     } catch (e) {
       responseMessage = e
+      console.log('createUser exception ', e)
     }
 
     const user = response
-    user.email = response?.email
-    user.firstName = response?.firstName
-    user.lastName = response?.lastName
-    user.phone = response?.phone
-    user.createdDate = response?.createdDate
+    if (response?.email) {
+      user.email = response?.email
+      user.firstName = response?.firstName
+      user.lastName = response?.lastName
+      user.phone = response?.phone
+      user.createdDate = response?.createdDate
+    } else {
+      user.error = response?.error
+    }
     user.responseMessage = responseMessage
+
     return user
   }
 }
